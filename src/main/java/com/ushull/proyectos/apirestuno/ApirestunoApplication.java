cb940fe982350d466a00cc56c1aa20254d170b21
Fernando Lopez Rodriguez <20203559@DG.intranet.scsalud.es>
2025-02-26 11:15:51 +0100

    commit inicial

---
+++ src/main/java/com/ushull/proyectos/apirestuno/ApirestunoApplication.java
@@ -0,0 +1,13 @@
+package com.ushull.proyectos.apirestuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ushull/proyectos/apirestuno/ApirestunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ushull.proyectos.apirestuno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
